7a2d0d1117b531e6efffd44f77527c6b8ebb6df9
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2025-12-08 15:11:20 -0800

    made clap detection more robust; added audio debugging; added dynamic threshold changing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <Arduino.h>
 
-constexpr int THRESHOLD = 3000;                // 12 bit adc 0-4095
-constexpr int TIME_WINDOW = 2000;              // time window to detect multiple claps (ms)
+//#define DEBUG_ENABLE
+#define DYNAMIC_THRESHOLD_ENABLE
+
+int THRESHOLD = 2500;                           // 12 bit adc 0-4095
+constexpr int TIME_WINDOW = 2000;               // time window to detect multiple claps (ms)
 constexpr int AUDIO_PIN = 15;
 constexpr int LED_PIN = 22;
 unsigned long clapBegin = millis();
 bool clapDetected = false;
 bool ledToggle = false;
 int clapCount = 0;
+
+#ifdef DEBUG_ENABLE
+// audio print timer for debugging
+// for now, every 200ms
+constexpr int PRINT_INTERVAL = 200;           // in ms
+unsigned long lastPrint = millis();
+#endif
 
 // put function declarations here:
 
@@ -19,36 +29,59 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(AUDIO_PIN);
+
+  #ifdef DEBUG_ENABLE
+  if (millis() - lastPrint > PRINT_INTERVAL) {      // print analog audio strength every PRINT_INTERVAL ms
+    lastPrint = millis();
+    uint16_t sensorValue = analogRead(AUDIO_PIN);
+    Serial.print("Audio Pin Value: ");
+    Serial.println(sensorValue);
+  }
+  #endif
+
+  #ifdef DYNAMIC_THRESHOLD_ENABLE
+  if (Serial.available()){                            // dynamic threshold changing
+    String command = Serial.readStringUntil('\n');
+    int newThreshold = command.toInt();
+    if (newThreshold > 0 && newThreshold <= 4095){
+      Serial.print("Setting new threshold to: ");
+      Serial.println(newThreshold);
+      THRESHOLD = newThreshold;
+    }
+  }
+  #endif
+
+
+  uint16_t sensorValue = analogRead(AUDIO_PIN);
   if (!clapDetected){
 
     if (sensorValue > THRESHOLD){
       clapBegin = millis();
       clapDetected = true;
       clapCount++;
-      Serial.println("Clap detected!");
-
-      delay(50);
+      
+      delay(100);
       sensorValue = analogRead(AUDIO_PIN);
       if (sensorValue > THRESHOLD){                 // sound source is not a clap
         clapDetected = false;
         clapCount = 0;
       }
+      else {
+        Serial.println("Clap detected!");
+      }
     }
 
   }
-  // read for 2s at 50ms intervals to see 
+  // read for 2s to see 
   // if continuous sound or if there are dips.
   // if there are dips in sound, it may be a clap.
   else {
     if (millis() - clapBegin < TIME_WINDOW){
-      delay(50);
       int sensorValue = analogRead(AUDIO_PIN);
       if (sensorValue > THRESHOLD){
         
         clapCount++;
-        delay(50);
+        delay(100);
         sensorValue = analogRead(AUDIO_PIN);
 
         if (sensorValue > THRESHOLD){                 // clap debounce
@@ -61,11 +94,14 @@ void loop() {
       Serial.print("Total claps detected: ");
       Serial.println(clapCount);
       clapDetected = false;
+      
+      if (clapCount > 1) {
+        ledToggle = ledToggle ? false : true;
+        Serial.print("Led On: ");
+        Serial.println(ledToggle ? "True" : "False");
+        digitalWrite(LED_PIN, ledToggle ? HIGH : LOW);
+      }
       clapCount = 0;
-      ledToggle = ledToggle ? false : true;
-      Serial.print("Led On: ");
-      Serial.println(ledToggle ? "True" : "False");
-      digitalWrite(LED_PIN, ledToggle ? HIGH : LOW);
     }
   }
 }
