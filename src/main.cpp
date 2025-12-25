a3a4e76f6432382e14afb65692b351c24114644c
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2025-12-24 19:41:16 -0800

    changed clap threshold to be a percentage above ambient nosie

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@
 #include <Arduino.h>
 #include <IRremote.hpp>
 
+#ifdef ARDUINO_ARCH_ESP32
+  constexpr int AUDIO_PIN = 15;
+#else
+  constexpr int AUDIO_PIN = A0;
+#endif
+
 /*Note to self:
   Fan: 32 bits Address: FF00
     Power: 0xEA15FF00 or 0x15
@@ -21,32 +27,30 @@
 
 #define DEBUG_ENABLE
 #define SERIAL_TOGGLE_ENABLE
-
-#ifdef ARDUINO_ARCH_ESP32
-  constexpr int AUDIO_PIN = 15;
-#else
-  constexpr int AUDIO_PIN = A0;
-#endif
+#define DYNAMIC_THRESHOLD_ENABLE
+//TODO: dynamic threshold percentage above ambient
 
 constexpr int TIME_WINDOW =             2000;       // time window to detect multiple claps (ms)
 constexpr int CLAP_DEBOUNCE =           30;         // time for a clap to fade (ms)
-constexpr float PERCENT_ABOVE_AVERAGE = 30/100.0f;  // threshold percentage above ema
 constexpr double ALPHA =                0.0000833;  // smoothing factor for ema                 
-double ema =                            0.0f;       // ~ one second of ema at 12kHz sample rate
 IRsend irsend;
 
 unsigned long clapBegin =               millis();
 bool clapDetected =                     false;
-bool ledToggle =                        false;
 int clapCount =                         0;
 long accumulator =                      0;
+int percentAboveAverage  =              30;         // threshold percentage above ema
+float decimalThreshold =                percentAboveAverage / 100.0f;  // the one used for calculations
+double ema =                            0.0f;       // ~ one second of ema at 12kHz sample rate
 
 #ifdef DEBUG_ENABLE
 constexpr int PRINT_INTERVAL =          200;           // in ms
 constexpr int COUNT_INTERVAL =          10000;         // in ms
+constexpr int LED_PIN =                 2;
 unsigned long lastPrint =               clapBegin;
 unsigned long lastCountPrint =          clapBegin;
 unsigned long cycles =                  0;
+bool ledToggle =                        false;
 #endif
 
 // put function declarations here:
@@ -88,6 +92,9 @@ void setup() {
   irsend.begin();
   Serial.begin(115200);
   EMAInit();
+  #ifdef DEBUG_ENABLE
+  pinMode(LED_PIN, OUTPUT);
+  #endif
 }
 
 void loop() {
@@ -96,7 +103,7 @@ void loop() {
   uint16_t originalValue = sensorValue;
   
   ema += ALPHA * (sensorValue - ema);                       // exponential moving average
-  const int THRESHOLD = ema * (1 + PERCENT_ABOVE_AVERAGE);  // dynamic threshold based on ema
+  const int THRESHOLD = ema * (1 + decimalThreshold);  // dynamic threshold based on ema
 
   #ifdef DEBUG_ENABLE
   unsigned long currentTime = millis();
@@ -109,25 +116,59 @@ void loop() {
     Serial.println(cycles);
     cycles = 0;
   }
-  if (currentTime - lastPrint > PRINT_INTERVAL) {                // print analog audio strength every PRINT_INTERVAL ms
+  if (currentTime - lastPrint > PRINT_INTERVAL)       // print analog audio strength every PRINT_INTERVAL ms
+  {                
     lastPrint = currentTime;
     Serial.print("Audio Pin Value: ");
     Serial.println(sensorValue);
     Serial.print("EMA: ");
     Serial.println(ema);
+    
+    switch (ledToggle) {
+      case true:
+        digitalWrite(LED_PIN, HIGH);
+        break;
+      default:
+        digitalWrite(LED_PIN, LOW);
+        break;
+    }
   }
+
   #endif
 
-  #ifdef SERIAL_TOGGLE_ENABLE
-  if (Serial.available())
-  {
+  #ifdef DYNAMIC_THRESHOLD_ENABLE
+    #ifdef SERIAL_TOGGLE_ENABLE
+    if (Serial.available())
+    {
+      String command = Serial.readStringUntil('\n');
+      if (command == "q") 
+      {
+        toggle();
+      } 
+      else
+      {
+        int newPercent = command.toInt();
+        if (newPercent > 0 && newPercent < 100)
+        {
+          percentAboveAverage = newPercent;
+          decimalThreshold = percentAboveAverage / 100.0f;
+          Serial.print("Threshold percentage set to: ");
+          Serial.println(newPercent);
+        }
+      }
+    }
+    #else
     String command = Serial.readStringUntil('\n');
-    if (command == "q") {
-      toggle();
-    } 
-  }
+    int newPercent = command.toInt();
+    if (newPercent > 0 && newPercent < 100)
+    {
+      percentAboveAverage = newPercent;
+      decimalThreshold = percentAboveAverage / 100.0f;
+      Serial.print("Threshold percentage set to: ");
+      Serial.println(newPercent);
+    }
+    #endif
   #endif
-
 
   if (!clapDetected)
   {
